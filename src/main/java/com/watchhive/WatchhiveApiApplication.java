928666a72b9ce2c0b69a9dec0797742a7b53dd33
bitturohit <rohit97750@gmail.com>
2025-08-12 17:01:40 +0530

    first commit

---
+++ src/main/java/com/watchhive/WatchhiveApiApplication.java
@@ -0,0 +1,16 @@
+package com.watchhive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchhiveApiApplication
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("heellooo");
+		SpringApplication.run(WatchhiveApiApplication.class, args);
+	}
+
+}
